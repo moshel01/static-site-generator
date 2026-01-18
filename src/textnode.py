38a18943b725ee66c8e07077528bb243e887b1bb
moshel01 <mos.hel0018@gmail.com>
2026-01-17 17:24:55 -0800

    added text_node_to_html_node

--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
 class TextType(Enum):
-    PLAIN = ""
-    BOLD = "****"
-    ITALIC = "__"
-    CODE = "''"
-    LINKS = "[]()"
-    IMAGES = "![]()"
+    TEXT = 'text'
+    BOLD = 'bold'
+    ITALIC = 'italic'
+    CODE = 'code'
+    LINK = 'link'
+    IMAGE = 'image'
 
 class TextNode():
     def __init__(self, text, text_type, url = None):
